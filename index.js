2891fdee0c55781a13abc09e3a7055b6d16dfc00
mnreis <mauriciotipo_0@hotmail.com>
2016-12-10 15:56:25 -0200

    up

--- index.js
+++ index.js
@@ -3,18 +3,9 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var config = require('./config/default.js');
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
-});
-
-app.get('/webhook', function (req, res) {
-    if (req.query['hub.verify_token'] === config.pageAccessToken) {
-      res.send(req.query['hub.challenge']);
-    } else {
-      res.send('Error, wrong validation token');    
-    }
 });
 
 app.listen(process.env.PORT || 8080);
