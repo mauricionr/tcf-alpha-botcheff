8bad50f1fdaf5529c09d97ef258b61d15210fcf3
mnreis <mauriciotipo_0@hotmail.com>
2016-12-10 16:54:38 -0200

    up

---
+++ config/actions.js
@@ -0,0 +1,20 @@
+module.exports = {
+    saudacao:"Olá!",
+    questao:"O que voce quer comer?",
+    categorias:{
+        PIZZA,
+        JAPONESA,
+        BURGER,
+        MASSA,
+        OUTRO
+    },
+    ordem:{
+        PRECO,
+        LOCALIZACAO,
+        AVALIACAO,
+    },
+    tipo_ordem:{
+        ASC,
+        DSC
+    }
+}
