d22100e8b97ad9326ad7f13d624e7d3c29a65b20
mnreis <mauriciotipo_0@hotmail.com>
2016-12-10 21:49:06 -0200

    up

--- index.js
+++ index.js
@@ -59,24 +59,28 @@ app.post('/webhook/', function (req, res) {
  *
  */
 function callSendAPI(messageData) {
-  return request({
-    uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: token },
-    method: 'POST',
-    json: messageData
-  }, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var recipientId = body.recipient_id;
-      var messageId = body.message_id;
-      if (messageId) {
-        console.log("Successfully sent message with id %s to recipient %s", messageId, recipientId);
-      } else {
-      console.log("Successfully called Send API for recipient %s", recipientId);
-      }
-    } else {
-      console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
-    }
-  });  
+  return new Promise((resolve, reject) => {
+        request({
+            uri: 'https://graph.facebook.com/v2.6/me/messages',
+            qs: { access_token: token },
+            method: 'POST',
+            json: messageData
+        }, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+            var recipientId = body.recipient_id;
+            var messageId = body.message_id;
+                if (messageId) {
+                    console.log("Successfully sent message with id %s to recipient %s", messageId, recipientId);
+                } else {
+                    console.log("Successfully called Send API for recipient %s", recipientId);
+                }
+                resolve(true)
+            } else {
+                console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
+                reject(false)
+            }
+        });  
+    })  
 }
 
 app.get('/', function (req, res) {
