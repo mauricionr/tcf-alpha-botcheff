de7f81aff5fa459d604ba8067eecb160a807cfab
mnreis <mauriciotipo_0@hotmail.com>
2016-12-10 15:54:01 -0200

    up

--- index.js
+++ index.js
@@ -3,43 +3,18 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var config = require('./config/default.js');
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.post('/webhook', function (req, res) {
-  var data = req.body;
-
-  // Make sure this is a page subscription
-  if (data.object === 'page') {
-
-    // Iterate over each entry - there may be multiple if batched
-    data.entry.forEach(function(entry) {
-      var pageID = entry.id;
-      var timeOfEvent = entry.time;
-
-      // Iterate over each messaging event
-      entry.messaging.forEach(function(event) {
-        if (event.message) {
-          receivedMessage(event);
-        } else {
-          console.log("Webhook received unknown event: ", event);
-        }
-      });
-    });
-
-    // Assume all went well.
-    //
-    // You must send back a 200, within 20 seconds, to let us know
-    // you've successfully received the callback. Otherwise, the request
-    // will time out and we will keep trying to resend.
-  }
+app.get('/webhook', function (req, res) {
+    if (req.query['hub.verify_token'] === config.pageAccessToken) {
+      res.send(req.query['hub.challenge']);
+    } else {
+      res.send('Error, wrong validation token');    
+    }
 });
-  
-function receivedMessage(event) {
-  // Putting a stub for now, we'll expand it in the following steps
-  console.log("Message data: ", event.message);
-}
 
 app.listen(process.env.PORT || 8080);
