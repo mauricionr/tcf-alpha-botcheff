b5e4183a9660c4fd068d49ad7d44eac07306b314
mnreis <mauriciotipo_0@hotmail.com>
2016-12-10 16:16:14 -0200

    up

---
+++ config/default.js
@@ -0,0 +1,6 @@
+module.exports = {
+    "appSecret": "07e60b4581bc50eaae1e4eed5449e075",
+    "pageAccessToken": "EAAFd8BpSnYUBALWZAh41bwsGPJmq7DgCzZB5q1ijn9PMWGvxsfzqosL9JxfDg3RyUMXdPXT6UzZA3fjQoZA7nZBZBytqRn7rWczlDi4MHYM7MEav3MEycWE6LvmoRZAlS3oZAr6JVqaMZAOmQZAxvT4qWAhOqjduHBMScdmtVRfRJWHwZDZD",
+    "validationToken": "",
+    "serverURL": "https://arcane-falls-74877.herokuapp.com/"
+}
