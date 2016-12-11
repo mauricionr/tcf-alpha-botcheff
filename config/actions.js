140b87cf57136348b0fd8dfe764973766876c5aa
mnreis <mauriciotipo_0@hotmail.com>
2016-12-11 03:42:42 -0200

    up

--- config/actions.js
+++ config/actions.js
@@ -352,7 +352,7 @@ module.exports = {
                                         "type":"web_url",
                                         "url":"https://arcane-falls-74877.herokuapp.com/masterpass",
                                         "title":"Efetuar pagamento",
-                                        "webview_height_ratio": "compact"
+                                        "webview_height_ratio": "full"
                                     }
                                 ]
                             }                            
@@ -387,7 +387,7 @@ module.exports = {
                                         "type":"web_url",
                                         "url":"https://arcane-falls-74877.herokuapp.com/masterpass",
                                         "title":"Efetuar pagamento",
-                                        "webview_height_ratio": "compact"
+                                        "webview_height_ratio": "full"
                                     }
                                 ]
                             }                            
