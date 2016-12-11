706c9ddef24b42d6d127e4eb28d73b90700153e6
mnreis <mauriciotipo_0@hotmail.com>
2016-12-10 22:09:47 -0200

    up

--- index.js
+++ index.js
@@ -21,10 +21,11 @@ app.post('/webhook/', function (req, res) {
 	for (let i = 0; i < messaging_events.length; i++) {
 		let event = req.body.entry[0].messaging[i]
         console.log('Event: ', event)
-        
+        let userId = event.sender.id;
 		let sender = event.sender.message_id
 
-        users[sender][carrinho] = users[sender][carrinho] || [] 
+        users[userId] = users[userId] || {};
+        users[userId][carrinho] = users[userId][carrinho] || [] 
 
 		if (event.message && event.message.text) {
             callSendAPI(actions.textMessage(sender, 'O que deseja comer hoje?')).then(() => {
